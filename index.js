dc2e512d9950f46fd2455d3839d649ca4004b267
Karl Yammine <karlyammine@Karls-MacBook-Air.local>
2025-07-29 17:36:42 +0300

    removed logs

--- index.js
+++ index.js
@@ -19,11 +19,9 @@ module.exports = async ({ req, res, log, error }) => {
     const formatted = `${day}/${month}/${year}`;
 
     const { receipt_id, status } = JSON.parse(req.body ?? '{}');
-    log(`Received update request for receiptId: ${receipt_id} and status: ${status}`);
 
     try {
         if (status === 'paid') {
-            log('started payment process')
             await databases.updateDocument(db, receipt_collection, receipt_id, {
                 status: 'paid',
                 date_paid: formatted
@@ -39,12 +37,9 @@ module.exports = async ({ req, res, log, error }) => {
                 Query.equal('date', receipt.month_key)
             ]);
             let unpaid = monthlyData.documents[0].unpaid - receipt.total;
-            log('unpaid is now: ' + unpaid)
             await databases.updateDocument(db, monthly_collection, monthlyData.documents[0].$id, {unpaid});
-            log('monthly data shoudl be updated')
 
             const newMonthlyData = await databases.listDocuments(db, monthly_collection, [Query.equal(date, formatted.split(-7))])
-            log(newMonthlyData)
             if (!newMonthlyData || newMonthlyData.total === 0) {
                 await databases.createDocument(db, monthly_collection, ID.unique(), {
                     date: formatted.split(-7),
@@ -52,7 +47,6 @@ module.exports = async ({ req, res, log, error }) => {
                     unpaid: 0
                 });
             } else {
-                log('trying existing monthly data to add paid info')
                 const existing = newMonthlyData.documents[0];
                 await databases.updateDocument(db, monthly_collection, existing.$id, {
                     paid: existing.paid + receipt_total
